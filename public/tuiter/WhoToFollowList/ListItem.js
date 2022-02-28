18407bf9c828617cbee5b616f4cfc522ff7502be
jackieder <der.ja@husky.neu.edu>
2022-02-28 10:30:50 -0500

    list module

--- public/tuiter/WhoToFollowList/ListItem.js
+++ public/tuiter/WhoToFollowList/ListItem.js
@@ -1,18 +1,9 @@
 const WhoToFollowListItem = (who) => {
-    let listItem = `
-<li class="list-group-item d-flex justify-content-start align-items-center">
-<div class="col-2"><img src="${who.avatarIcon}"class="wd-profile-pic wd-img-invert" alt="..."></div>
-                <div class="col-7 pt-1 pb-0 px-0"><p class="h6 text-white">${who.userName} <i
-                        class="fa fa-check-circle-o"></i> <span class="h6 text-white-50"></span>
-                </p><p class="h6 override-bs">@${who.handle}</p>
-                </div>
-                <div class="col-3 p-0">
-                    <button class="btn btn-light fw-bold rounded-pill" type="button">Follow</button>
-                </div>
-
-</div></li>`
-
-    return listItem
+   return (`
+   <div class="list-group-item justify-content-start align-items-center">
+   <img src="${who.avatarIcon}"><p>${who.userName}</p><p>${who.handle}</p>
+   </div>
+   `);
 }
 
 export default WhoToFollowListItem;
--- public/tuiter/WhoToFollowList/index.js
+++ public/tuiter/WhoToFollowList/index.js
@@ -2,9 +2,14 @@ import { default as who } from "./who.js";
 import WhoToFollowListItem from "./ListItem.js";
 
 const WhoToFollowList = () => {
-    let WhoToFollowList = `<div class="list-group"><li><p>`;
-
-    WhoToFollowList.concat(`</p></li></div>`);
-    return WhoToFollowList;
+    return(`
+<div class="list-group">
+ ${
+        who.map(account => {
+            return(WhoToFollowListItem(account));
+        }).join('')
+    }
+</div> `);
 }
+
 export default WhoToFollowList
