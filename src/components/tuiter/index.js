d196ac7c21a6436c97b3d56e6e4c8774ef715719
jackieder <der.ja@husky.neu.edu>
2022-03-21 18:02:07 -0400

    comment out unused imports

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import ExploreScreen from "./ExploreScreen"
-import HomeScreen from "./HomeScreen"
+// import React from "react";
+// import {Link} from "react-router-dom";
+// import ExploreScreen from "./ExploreScreen"
+// import HomeScreen from "./HomeScreen"
 
 const Tuiter = () => {
     return(
