feee55727699d5379c93d76f1157008e48d5b12d
jackieder <der.ja@husky.neu.edu>
2022-03-28 22:53:16 -0400

    clean up explore page

--- src/components/tuiter/ExploreScreen.js
+++ src/components/tuiter/ExploreScreen.js
@@ -8,17 +8,7 @@ import ExploreComponent from "./explore-screen";
 const ExploreScreen = () => {
     return (
         <>
-            <div className="row mt-2">
-                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                    <NavigationSidebar active="Explore"/>
-                </div>
-                <div className="col-10 col-md-10 col-lg-7 col-xl-6">
-                    <ExploreComponent/>
-                </div>
-                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                    <WhoToFollowList/>
-                </div>
-            </div>
+            <ExploreComponent/>
             <Link to="/hello">
                 Hello
             </Link> |
