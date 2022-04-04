59766ef0a8c8c7fb6afa416b19c57a882d529e13
jackieder <der.ja@husky.neu.edu>
2022-04-03 20:54:25 -0400

    push to a8

---
+++ src/services/tuits-service.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+const TUITS_API = 'http://localhost:4000/api/tuits';
+
+export const createTuit = async (tuit) => {}
+export const findAllTuits = async () => {}
+export const deleteTuit = async (tuit) => {}
+export const updateTuit = async (tuit) => {}
