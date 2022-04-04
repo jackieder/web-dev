04218f2f755c811924255981696d0d6bddf0cfec
jackieder <der.ja@husky.neu.edu>
2022-04-03 22:28:45 -0400

    fix tuits api

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TUITS_API = 'https://full-stack-developer-server.herokuapp.com/api/tuits';
+const TUITS_API = 'https://full-stack-developer-node.herokuapp.com/api/tuits';
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
